ebf62b8c90c29ca746a58017024b5ff7d1815b70
shekhar771 <ashekhar.1239@gmail.com>
2023-01-02 12:03:38 +0530

    added Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -15,10 +15,9 @@ import {
 
 const App = () => {
   return (
-    <div className='bg-primary w-full overflow-hidden '>
+    <div className=' bg-primary w-full overflow-hidden '>
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
-          Navbar
           <Navbar />
         </div>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,55 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { close, menu, logo } from '../assets/assets';
+import { navLinks } from '../constants';
 const Navbar = () => {
-  return <div></div>;
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <nav className='w-full flex py-6 justify-between items-center navbar'>
+      <img className='w-[132px] h-[32px]' src={logo} alt='hoobank' />
+      <ul
+        className={`list-none sm:flex hidden justify-end items-center flex-1`}
+      >
+        {navLinks.map((nav, index) => (
+          <li
+            className={`font-poppins font-normal cursor-pointer text-[16px] ${
+              index === navLinks.length - 1 ? 'mr-0' : 'mr-10'
+            } text-white`}
+            key={nav.id}
+          >
+            <a href={`#${nav.id}`}>{nav.title}</a>
+          </li>
+        ))}
+      </ul>
+      <div className='sm:hidden flex flex-1 justify-end items-center'>
+        <img
+          src={toggle ? close : menu}
+          alt='menu'
+          className='w-[28px] h-[28px] object-contain '
+          onClick={() => setToggle((prev) => !prev)}
+        />
+        <div
+          className={`${
+            toggle ? 'flex' : 'hidden'
+          }  p-6  bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        >
+          <ul
+            className={`list-none flex flex-col justify-end items-center flex-1`}
+          >
+            {navLinks.map((nav, index) => (
+              <li
+                className={`font-poppins font-normal cursor-pointer text-[16px] mb-4
+                } text-white`}
+                key={nav.id}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
